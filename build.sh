8fb42fc4127236457d53e08a70e1ffb60bce6c62
Yehiyam Livneh <yehiyam@gmail.com>
2020-03-04 17:37:10 +0200

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 VERSION=v${PACKAGE_VERSION}
 docker build -t hkube/oc-builder:${VERSION} .
-# docker push hkube/oc-builder:${VERSION}
+docker push hkube/oc-builder:${VERSION}
